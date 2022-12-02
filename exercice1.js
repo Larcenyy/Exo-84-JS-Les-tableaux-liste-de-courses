8d4e3a6cf7061d09b67dbc288f38510f371cc0cd
Rémy <remyc59440@hotmail.com>
2022-12-02 17:40:21 +0100

    m

--- exercice1.js
+++ exercice1.js
@@ -1,4 +1,9 @@
-// Inutile de modifier le code suivant
+let elem = document.getElementById('listeCourse');
+let send = document.getElementById("send");
+let item = document.getElementById("item");
+let del = document.getElementById("del");
+
+
 let articles = [
     "Orangina",
     "Creme Fraiche",
@@ -10,12 +15,22 @@ let articles = [
     "Pizza"
 ];
 
-// Votre code à partir d'ici :
+
+for (let obj of articles){
+
+    let list = document.createElement('li')
+
+    list.innerHTML = obj;
+    elem.appendChild(list)
+
+    send.addEventListener('click', function (){
+        let add = document.createElement('li');
+        list.lastChild(add)
+        add.innerHTML = item.value
+    })
 
 
-
-
-
-
-
-
+    del.addEventListener("click", function (){
+        list.innerHTML += "test"
+    })
+}
